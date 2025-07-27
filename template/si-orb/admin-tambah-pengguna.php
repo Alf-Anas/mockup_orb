d9f9afbd50acae12d64d899189f48773c30254d7
Alf-Anas <alfadila.anas@gmail.com>
2025-07-27 17:54:34 +0700

    update admin list role

--- template/si-orb/admin-tambah-pengguna.php
+++ template/si-orb/admin-tambah-pengguna.php
@@ -5,7 +5,30 @@
   ?>
 
  <div class="page-body">
-   <div class="container-fluid pt-4">
+   <div class="container-fluid">
+     <div class="page-title">
+       <div class="row">
+         <div class="col-sm-6">
+           <h3>Tambah Pengguna</h3>
+         </div>
+         <div class="col-sm-6">
+           <ol class="breadcrumb">
+             <li class="breadcrumb-item">
+               <a href="./beranda.php">
+                 <i data-feather="home"></i>
+               </a>
+             </li>
+             <li class="breadcrumb-item">Admin</li>
+             <li class="breadcrumb-item active">
+               Tambah Pengguna
+             </li>
+           </ol>
+         </div>
+       </div>
+     </div>
+   </div>
+
+   <div class="container-fluid">
      <div class="row">
        <div class="col-sm-12">
          <div class="card">
@@ -63,16 +86,64 @@
                    <div class="form-group">
                      <label class="col-form-label">Hak Akses</label>
                      <div class="form-check">
-                       <input class="form-check-input" type="checkbox" name="akses[]" value="RBBK:VIEW" id="akses1">
-                       <label class="form-check-label" for="akses1">RBBK:VIEW</label>
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="ADMIN:MANAGE-USER" id="akses3">
+                       <label class="form-check-label" for="akses3">ADMIN:MANAGE-USER</label>
                      </div>
                      <div class="form-check">
-                       <input class="form-check-input" type="checkbox" name="akses[]" value="RBBK:EDIT" id="akses2">
-                       <label class="form-check-label" for="akses2">RBBK:EDIT</label>
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="KELEMBAGAAN:PE-PD" id="akses3">
+                       <label class="form-check-label" for="akses3">KELEMBAGAAN:PE-PD</label>
                      </div>
                      <div class="form-check">
-                       <input class="form-check-input" type="checkbox" name="akses[]" value="PETA:ADMIN" id="akses3">
-                       <label class="form-check-label" for="akses3">PETA:ADMIN</label>
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="KELEMBAGAAN:PE-ORB" id="akses3">
+                       <label class="form-check-label" for="akses3">KELEMBAGAAN:PE-ORB</label>
+                     </div>
+                     <div class="form-check">
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="KELEMBAGAAN:KO-PD" id="akses3">
+                       <label class="form-check-label" for="akses3">KELEMBAGAAN:KO-PD</label>
+                     </div>
+                     <div class="form-check">
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="KELEMBAGAAN:KO-ORB" id="akses3">
+                       <label class="form-check-label" for="akses3">KELEMBAGAAN:KO-ORB</label>
+                     </div>
+                     <div class="form-check">
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="ANJAB:PD-OPERATOR" id="akses3">
+                       <label class="form-check-label" for="akses3">ANJAB:PD-OPERATOR</label>
+                     </div>
+                     <div class="form-check">
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="ANJAB:PD-PIMPINAN" id="akses3">
+                       <label class="form-check-label" for="akses3">ANJAB:PD-PIMPINAN</label>
+                     </div>
+                     <div class="form-check">
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="ANJAB:ORB-VALIDATOR" id="akses3">
+                       <label class="form-check-label" for="akses3">ANJAB:ORB-VALIDATOR</label>
+                     </div>
+                     <div class="form-check">
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="ANJAB:ORB-PIMPINAN" id="akses3">
+                       <label class="form-check-label" for="akses3">ANJAB:ORB-PIMPINAN</label>
+                     </div>
+                     <div class="form-check">
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="RBBK:OPERATOR" id="akses2">
+                       <label class="form-check-label" for="akses2">RBBK:OPERATOR</label>
+                     </div>
+                     <div class="form-check">
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="RBBK:EDITOR" id="akses2">
+                       <label class="form-check-label" for="akses2">RBBK:EDITOR</label>
+                     </div>
+                     <div class="form-check">
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="RBBK:VERIFIKATOR" id="akses1">
+                       <label class="form-check-label" for="akses1">RBBK:VERIFIKATOR</label>
+                     </div>
+                     <div class="form-check">
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="RBBK:PIMPINAN" id="akses2">
+                       <label class="form-check-label" for="akses2">RBBK:PIMPINAN</label>
+                     </div>
+                     <div class="form-check">
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="TLPP:PD" id="akses3">
+                       <label class="form-check-label" for="akses3">TLPP:PD</label>
+                     </div>
+                     <div class="form-check">
+                       <input class="form-check-input" type="checkbox" name="akses[]" value="TLPP:ORB" id="akses3">
+                       <label class="form-check-label" for="akses3">TLPP:ORB</label>
                      </div>
                    </div>
                  </div>
